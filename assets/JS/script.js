9033596e342d9eeb6edb78d62ee4fca9901d0d8c
Cam <camhack67@gmail.com>
2023-08-04 19:04:56 -0400

    added basic weather fetch

---
+++ assets/JS/script.js
@@ -0,0 +1,22 @@
+let weatherGeoAPI = 'https://api.openweathermap.org/geo/1.0/direct?q=pittsburgh&limit=5&appid=e6b1fac811d8aab3c6db94ce80eb868d'
+
+
+fetch(weatherGeoAPI)
+    .then(response => response.json())
+    .then(citiesFound => {
+        let firstCity = citiesFound[0]
+        console.log(firstCity.lat)
+        console.log(firstCity.lon)
+
+        let weatherLocationAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=e6b1fac811d8aab3c6db94ce80eb868d`
+        return fetch(weatherLocationAPI)
+    }) 
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+
+
+
+
+
